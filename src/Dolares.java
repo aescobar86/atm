21b2ddddf8ca0c17797328f483326031aeddc7ee
Diego <dmgp.91@gmail.com>
2023-03-24 16:41:17 -0600

    Agregando funcionalidad de dolares

---
+++ src/Dolares.java
@@ -0,0 +1,25 @@
+public class Dolares {
+    private int tasaCambio = 36;
+    private Cuenta cuenta;
+
+    public Dolares(Cuenta cordoba) {
+        this.cuenta = cordoba;
+    }
+
+    public double getSaldo() {
+        return cuenta.getSaldo() / this.tasaCambio;
+    }
+
+    public void depositar(double monto) {
+        cuenta.depositar(this.tasaCambio * monto);
+    }
+
+    public void retirar(double monto) {
+        cuenta.retirar(this.tasaCambio * monto);
+    }
+
+    @Override
+    public String toString() {
+       return "El saldo en dólares es: " + this.getSaldo();
+    }
+}
